8cb644352ce11a43d042a522f48ac13e85a8ad87
hdd Prayuda <hadidpray12@gmail.com>
2021-03-11 14:33:50 +0700

    commit bottom navigation android kotlin git

--- app/src/main/java/com/example/tokoonline/MainActivity.kt
+++ app/src/main/java/com/example/tokoonline/MainActivity.kt
@@ -2,11 +2,64 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.tokoonline.fargment.AkunFragment
+import com.example.tokoonline.fargment.HomeFragment
+import com.example.tokoonline.fargment.KeranjangFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+  class MainActivity : AppCompatActivity() {
+
+      val  fargmentHome : Fragment = HomeFragment()
+      val  fargmentAkun : Fragment = AkunFragment()
+      val  fargmentKeranjang : Fragment = KeranjangFragment()
+      val fm : FragmentManager = supportFragmentManager
+      var active : Fragment = fargmentHome
+
+      private lateinit var menu: Menu
+      private lateinit var menuItem: MenuItem
+      private lateinit var bottomNavigationView : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpBottomNav()
     }
+      fun setUpBottomNav(){
+
+          fm.beginTransaction().add(R.id.container, fargmentHome).show(fargmentHome).commit()
+          fm.beginTransaction().add(R.id.container, fargmentAkun).hide(fargmentAkun).commit()
+          fm.beginTransaction().add(R.id.container, fargmentKeranjang).hide(fargmentKeranjang).commit()
+
+          bottomNavigationView = findViewById(R.id.nav_view)
+          menu        = bottomNavigationView.menu
+          menuItem    = menu.getItem(0)
+          menuItem.isChecked = true
+
+          bottomNavigationView.setOnNavigationItemSelectedListener {
+                  item ->
+              when(item.itemId){
+                  R.id.navigation_home->{
+                      callFragment(0, fargmentHome)
+                  }
+                  R.id.navigation_keranjang->{
+                      callFragment(1, fargmentKeranjang)
+                  }
+                  R.id.navigation_akun->{
+                      callFragment(2, fargmentAkun)
+                  }
+              }
+              false
+          }
+      }
+      fun callFragment(int: Int, fragment: Fragment){
+          menuItem    = menu.getItem(int)
+          menuItem.isChecked = true
+          fm.beginTransaction().hide(active).show(fragment).commit()
+          active = fragment
+      }
 }
